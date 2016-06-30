a95ad71337004344713f5ff3afff353a7dc181b5
gdnwxf <gdnwxf@qq.com>
2016-06-30 14:03:30 +0800

    测试上传的用户名

--- framework/src/test/java/com/wch/pojo/parser/MyTestTest.java
+++ framework/src/test/java/com/wch/pojo/parser/MyTestTest.java
@@ -1,7 +1,5 @@
 package com.wch.pojo.parser;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by wch on 2016/5/22.
  */
@@ -10,7 +8,7 @@ public class MyTestTest {
 
     public static MyTestTest getInstance() {
         MyTestTest dsadsa = new MyTestTest();
-        return dsadsa;  
+        return dsadsa;
     }
 
 
