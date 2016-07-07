03d7512e68b132cec5faf4132bdf5fe828817215
qinghao <qinghao@2dfire.com>
2016-07-07 15:46:08 +0800

    velocityutils

--- framework/src/main/java/com/wch/velocity/VelocityUtils.java
+++ framework/src/main/java/com/wch/velocity/VelocityUtils.java
@@ -1,10 +1,16 @@
 package com.wch.velocity;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.List;
 import java.util.Properties;
 
+import org.apache.velocity.Template;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
 import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.directive.Macro;
 import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
 
 public class VelocityUtils {
@@ -16,8 +22,14 @@ public class VelocityUtils {
 		Velocity.init(ve);
 	}
 	
-	public static void  Single(VelocityContext ctx, ) {
-		Velocity.
+	public static void  Single(VelocityContext ctx, Writer writer ,String templateName) {
+		 Single(ctx, writer, templateName, null );
 	}
-
+	public static void  Single(VelocityContext ctx, Writer writer ,String templateName,List<Macro> list) {
+		 Template t = Velocity.getTemplate(templateName);
+		 t.merge(ctx, writer, list);
+	}
+	public static void  Single(VelocityContext ctx, String fileName ,String templateName) throws IOException {
+		 Single(ctx, new FileWriter(fileName), null );
+	}
 }
