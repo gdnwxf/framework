f1c641fc241a4986541760ef0489d4513a743b50
gdnwxf <1156591472@qq.com>
2016-08-29 00:08:59 +0800

    添加disruptor 的测试

--- framework/src/main/java/com/wch/test/disruptor/PublishEvent.java
+++ framework/src/main/java/com/wch/test/disruptor/PublishEvent.java
@@ -4,15 +4,14 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import com.lmax.disruptor.EventFactory;
-import com.lmax.disruptor.EventHandler;
+import com.lmax.disruptor.RingBuffer;
 import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
 import com.lmax.disruptor.dsl.ProducerType;
 import com.wch.test.disruptor.disruptor.LongEvent;
 import com.wch.test.disruptor.disruptor.LongEventFactory;
-import com.wch.test.disruptor.disruptor.LongEventHandler;
 
-public class DisruptorTest {
+public class PublishEvent {
 
 	public static void main(String[] args) {
 		EventFactory<LongEvent> eventFactory = new LongEventFactory();
@@ -22,10 +21,20 @@ public class DisruptorTest {
 		Disruptor<LongEvent> disruptor = new Disruptor<LongEvent>(eventFactory,
 		                ringBufferSize, executor, ProducerType.SINGLE,
 		                new YieldingWaitStrategy());
-		        
-		EventHandler<LongEvent> eventHandler = new LongEventHandler();
-		disruptor.handleEventsWith(eventHandler);
-		        
-		disruptor.start();
+		// 发布事件；
+		RingBuffer<LongEvent> ringBuffer = disruptor.getRingBuffer();
+		long sequence = ringBuffer.next();//请求下一个事件序号；
+		    
+		try {
+		    LongEvent event = ringBuffer.get(sequence);//获取该序号对应的事件对象；
+		    long data = getEventData();//获取要通过事件传递的业务数据；
+		    event.set(data);
+		} finally{
+		    ringBuffer.publish(sequence);//发布事件；
+		}
+	}
+
+	private static long getEventData() {
+		return 0;
 	}
 }
---
+++ framework/src/main/java/com/wch/test/disruptor/disruptor/LongEvent.java
@@ -0,0 +1,10 @@
+package com.wch.test.disruptor.disruptor;
+public class LongEvent
+{
+    private long value;
+
+    public void set(long value)
+    {
+        this.value = value;
+    }
+}
