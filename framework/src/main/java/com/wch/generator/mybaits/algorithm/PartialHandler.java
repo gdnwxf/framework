a26668ede4338597f714153fb892e3c01c65dcbf
wch <wch@192.16.0.119>
2017-05-06 10:47:58 +0800

    添加 算法

---
+++ framework/src/main/java/com/wch/generator/mybaits/algorithm/PartialHandler.java
@@ -0,0 +1,7 @@
+package com.wch.generator.mybaits.algorithm;
+
+import java.util.List;
+
+public interface PartialHandler<T> {
+	Integer handle(List<T> list) ;
+}
---
+++ framework/src/main/java/com/wch/generator/mybaits/algorithm/PartialList.java
@@ -0,0 +1,35 @@
+package com.wch.generator.mybaits.algorithm;
+
+import java.util.List;
+
+
+public class PartialList {
+
+	public static <T extends Object> int cyclicHandle(List<T> list ,Integer step , PartialHandler<T> partialHandler){
+		int totalSize = list.size();
+		step = (step == null || step < 0 ) ? 30 : step;
+		int totalRefectRow = 0 ;
+		int resultRow = 0 ;
+		int fromIndex = 0;
+		int toIndex = step;
+		
+		if(totalSize <= step) {
+			  resultRow = partialHandler.handle(list);
+			  totalRefectRow += resultRow;
+		}else {
+			//当当前的fromIndex　<＝ 总size的时候结束循环
+			while ( fromIndex < totalSize ) {
+				List<T> subList = list.subList(fromIndex, toIndex);
+				resultRow = partialHandler.handle(subList);
+				totalRefectRow += resultRow;
+				//每次添加相同的步长
+				fromIndex = fromIndex + step;
+				toIndex = toIndex + step;
+				if(toIndex > totalSize) { toIndex = totalSize; }
+			}
+		}
+		
+		return totalRefectRow;
+	}
+}
+
